0b103156521e8b6ddeaae4cde82aab53f91bbd23
Matteus888 <matthieu888@gmail.com>
2026-05-03 10:02:48 +0200

    Fix Header issue

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -6,8 +6,6 @@ import SearchBar from "@/components/ui/SearchBar/SearchBar";
 import NavBar from "@/components/layout/NavBar/NavBar";
 import ProfileMenuAvatar from "../ProfileMenuAvatar/ProfileMenuAvatar";
 import Link from "next/link";
-import Icon from "@mdi/react";
-import { mdiMagnify } from "@mdi/js";
 
 export default function Header() {
   return (
@@ -16,7 +14,6 @@ export default function Header() {
         <Link className={styles.logoLink} href="/">
           <Logo size={35} animated />
         </Link>
-        {/* <Icon path={mdiMagnify} size={1.35} /> */}
         <SearchBar />
       </div>
       <div className={styles.rightContainer}>
