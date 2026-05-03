264035eb8a632c367bc8f81821c761c086eae597
Matteus888 <matthieu888@gmail.com>
2026-05-03 09:52:47 +0200

    Change ProfileMenuDropdown, DynamicSearchResult & SearchBar style

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -6,6 +6,8 @@ import SearchBar from "@/components/ui/SearchBar/SearchBar";
 import NavBar from "@/components/layout/NavBar/NavBar";
 import ProfileMenuAvatar from "../ProfileMenuAvatar/ProfileMenuAvatar";
 import Link from "next/link";
+import Icon from "@mdi/react";
+import { mdiMagnify } from "@mdi/js";
 
 export default function Header() {
   return (
@@ -14,6 +16,7 @@ export default function Header() {
         <Link className={styles.logoLink} href="/">
           <Logo size={35} animated />
         </Link>
+        {/* <Icon path={mdiMagnify} size={1.35} /> */}
         <SearchBar />
       </div>
       <div className={styles.rightContainer}>
